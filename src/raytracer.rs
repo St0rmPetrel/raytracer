d28e8868eb38e3aa281aaa5313dfedb88d13d3a9
Telman Rzaev <rzaev.telman.tarik@gmail.com>
2023-06-16 20:13:22 +0300

    Add config any resolution

--- src/raytracer.rs
+++ src/raytracer.rs
@@ -4,26 +4,36 @@ use crate::scene::Scene;
 
 pub fn fill_image(image: &mut RasterImage, scene: Scene) {
     let (width, height) = image.get_resolution();
-    let resolution = std::cmp::max(width, height) + 1;
+    let resolution = std::cmp::max(width, height);
 
-    let mut convas = Canvas::new(15.0, resolution);
+    let mut convas = Canvas::new(15.0, resolution + 1);
 
-    convas.fill_canvas(scene, (0, 1281), (280, 1001));
+    let (h_shift, w_shift) = if height < width {
+        ((resolution - height) / 2, 0)
+    } else {
+        (0, (resolution - width) / 2)
+    };
+
+    convas.fill_canvas(
+        scene,
+        (w_shift, resolution - w_shift + 1),
+        (h_shift, resolution - h_shift + 1),
+    );
 
     for j in 0..height {
         for i in 0..width {
             let pixel = image.get_pixel(i, j).expect("pixel in image not found");
             let color_ul = convas
-                .get_canvas_pixel(i, j + 280)
+                .get_canvas_pixel(i + w_shift, j + h_shift)
                 .expect("pixel in convas not found");
             let color_ur = convas
-                .get_canvas_pixel(i + 1, j + 280)
+                .get_canvas_pixel(i + w_shift + 1, j + h_shift)
                 .expect("pixel in convas not found");
             let color_bl = convas
-                .get_canvas_pixel(i, j + 281)
+                .get_canvas_pixel(i + w_shift, j + h_shift + 1)
                 .expect("pixel in convas not found");
             let color_br = convas
-                .get_canvas_pixel(i + 1, j + 281)
+                .get_canvas_pixel(i + w_shift + 1, j + h_shift + 1)
                 .expect("pixel in convas not found");
             let color_u = color_ul + color_ur;
             let color_b = color_bl + color_br;
